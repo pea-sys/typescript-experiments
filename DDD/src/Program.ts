a6389aaf3dee6752cc30f3b35d4b506df99e75b6
masami <pea98258@gmail.com>
2024-04-17 06:53:09 +0900

    feat:DDD

---
+++ DDD/src/Program.ts
@@ -0,0 +1,33 @@
+import { container, Lifecycle } from "tsyringe";
+
+import { PrismaBookRepository } from "Infrastructure/Prisma/Book/PrismaBookRepository";
+import { PrismaClientManager } from "Infrastructure/Prisma/PrismaClientManager";
+import { PrismaTransactionManager } from "Infrastructure/Prisma/PrismaTransactionManager";
+import { EventEmitterDomainEventPublisher } from "Infrastructure/DomainEvent/EventEmitter/EventEmitterDomainEventPublisher";
+import { EventEmitterDomainEventSubscriber } from "Infrastructure/DomainEvent/EventEmitter/EventEmitterDomainEventSubscriber";
+// repository
+container.register("IBookRepository", {
+  useClass: PrismaBookRepository,
+});
+
+// transactionManager
+container.register("ITransactionManager", {
+  useClass: PrismaTransactionManager,
+});
+
+// IDataAccessClientManager
+container.register(
+  "IDataAccessClientManager",
+  {
+    useClass: PrismaClientManager,
+  },
+  // The same instance will be resolved for each resolution of this dependency during a single resolution chain
+  { lifecycle: Lifecycle.ResolutionScoped }
+);
+// DomainEvent
+container.register("IDomainEventPublisher", {
+  useClass: EventEmitterDomainEventPublisher,
+});
+container.register("IDomainEventSubscriber", {
+  useClass: EventEmitterDomainEventSubscriber,
+});
